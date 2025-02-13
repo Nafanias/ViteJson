57dcacfcae53a4fa221e9dc0500db91be3df76cb
Nafanias <vag57205@gmail.com>
2025-02-13 20:55:57 +0300

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  base: '/ViteJson/', // ← Важно: слеш в конце!
+  build: {
+    outDir: 'docs' // Для GitHub Pages
+  }
+});
